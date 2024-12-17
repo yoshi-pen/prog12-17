8327d881824300fcd7b335293e3b708b23c9bd87
Masaki Yoshikawa <j2370rgg@sit.ac.jp>
2024-12-17 16:33:14 +0900

    代入しなおし

--- models.py
+++ models.py
@@ -23,12 +23,12 @@ class Mymodel(nn.Module):
 def test_accuracy(model, dataloader, device="cpu"):
     n_corrects = 0#正解の個数
 
-    model.to(device)
+    model = model.to(device)
 
     model.eval()
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
         with torch.no_grad():
             logits_batch = model(image_batch)
 
@@ -42,11 +42,11 @@ def test_accuracy(model, dataloader, device="cpu"):
 
 def train(model,dataloader,loss_fn,optimizer, device="cpu"):
     "1epochの学習"
-    model.to(device)
+    model = model.to(device)
     model.train()
     for image_batch, label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
         logits_batch = model(image_batch)
 
         loss = loss_fn(logits_batch, label_batch)
@@ -60,11 +60,11 @@ def train(model,dataloader,loss_fn,optimizer, device="cpu"):
 def test(model,dataloader,loss_fn, device="cpu"):
     loss_total = 0.0
 
-    model.to(device)
+    model = model.to(device)
     model.eval()
     for image_batch,label_batch in dataloader:
-        image_batch.to(device)
-        label_batch.to(device)
+        image_batch = image_batch.to(device)
+        label_batch = label_batch.to(device)
         with torch.no_grad():
             logits_batch = model(image_batch)
 
